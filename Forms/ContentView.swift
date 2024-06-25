f67ef0e8009574bbadef43f29fc6ec3f5c366ea7
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-25 19:24:14 +0400

    Initial Commit

---
+++ Forms/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Forms
+//
+//  Created by Emmanuelle  Dennemont on 25/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Forms/FormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormsApp.swift
+//  Forms
+//
+//  Created by Emmanuelle  Dennemont on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
